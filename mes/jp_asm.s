1a44e95d730f70054a73d869ef2aee36fa1da40e
Jainish <Jpatel7648@conestogac.on.ca>
2023-05-17 21:08:02 -0400

    Final commit with lab 3 done

--- mes/jp_asm.s
+++ mes/jp_asm.s
@@ -13,6 +13,7 @@
 .global add_test @ Make the symbol name for the function visible to the linker
 .code 16 @ 16bit THUMB code (BOTH .code and .thumb_func are required)
 .thumb_func @ Specifies that the following symbol is the name of a THUMB
+
 @ encoded function. Necessary for interlinking between ARM and THUMB code.
 .type add_test, %function @ Declares that the symbol is a function (not strictly required)
 @ Function Declaration : int add_test(int x, int y)
@@ -22,8 +23,33 @@
 @
 @ Here is the actual add_test function
 add_test:
+push {lr}
 add r0, r0, r1
+mov r0, #3
+bl BSP_LED_Toggle
+mov r0, r2
+bl busy_delay
+pop {lr}
 bx lr @ Return (Branch eXchange) to the address in the link register (lr)
 .size add_test, .-add_test @@ - symbol size (not strictly required, but makes the debugger happy)
+
+
+@ Function Declaration : int busy_delay(int cycles)
+@
+@ Input: r0 (i.e. r0 holds number of cycles to delay)
+@ Returns: r0
+@
+@ Here is the actual function. DO NOT MODIFY THIS FUNCTION.
+busy_delay:
+push {r5}
+mov r5, r0
+delay_1oop:
+subs r5, r5, #1
+bge delay_1oop
+mov r0, #0 @ Return zero (success)
+pop {r5}
+bx lr @ Return (Branch eXchange) to the address in the link register (lr)
+
+
 @ Assembly file ended by single .end directive on its own line
 .end
--- mes/jp_hook.c
+++ mes/jp_hook.c
@@ -6,16 +6,23 @@
 #include <stdint.h>
 #include <ctype.h>
 #include "common.h"
-int add_test(int x, int y);
+int add_test(int x, int y, int del);
 void AddTest(int action)
 {
 if(action==CMD_SHORT_HELP) return;
 if(action==CMD_LONG_HELP) {
-printf("Addition Test\n\n"
-"This command tests new addition function\n"
-);
+printf("Addition Test\n\n" "This command tests new addition function\n");
 return;
 }
-printf("add_test returned: %d\n", add_test(99, 87) );
+uint32_t delay;
+int fetch_status;
+fetch_status = fetch_uint32_arg(&delay);
+if(fetch_status) {
+// Use a default delay value
+delay = 0xFFFFFF;
+}
+// When we call our function, pass the delay value.
+// printf(“<<< here is where we call add_test – can you add a third parameter? >>>”);
+printf("add_test returned: %d\n", add_test(99, 87, delay));
 }
 ADD_CMD("add", AddTest,"Test the new add function")
